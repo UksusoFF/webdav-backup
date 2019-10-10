2c2a3cbb83ccf3b67ee38e72cfc2e5ecb2e170f1
Uksusov Kirill <uksusoff@yandex.ru>
2019-10-10 16:25:56 +0400

    Debug issues fixed

--- run.sh
+++ run.sh
@@ -8,22 +8,20 @@ ARCFOLDERNAME=$(date '+%F_%H_%M')
 if [ ! -f "$SCRIPT_DIR/config.sh" ]; then
   cp "$SCRIPT_DIR/config.sh.example" "$SCRIPT_DIR/config.sh"
   cp "$SCRIPT_DIR/include.list.example" "$SCRIPT_DIR/include.list"
-  touch prev.list
+  touch "$SCRIPT_DIR/prev.list"
   echo "Place your credentials to config.sh and file paths to include.list"
   exit 1
 fi
 
-source "./config.sh"
+source "$SCRIPT_DIR/config.sh"
 
 echo "Local available space: $(df -h . | awk 'END {print $4}')"
 echo "Local used space: $(df -h . | awk 'END {print $5}')"
 
 REMOTE_SPACE_INFO=$(curl -i --request PROPFIND --header "Depth: 0" --data-ascii "<D:propfind xmlns:D=\"DAV:\"><D:prop><D:quota-available-bytes/><D:quota-used-bytes/></D:prop></D:propfind>" --user "$WEBDAVUSER":"$WEBDAVPASS" "$WEBDAVURL" --silent --show-error)
-REMOTE_AVAILABLE_SPACE=$(ggrep -oPm1 "(?<=<d:quota-available-bytes>)[^<]+" <<<"$REMOTE_SPACE_INFO" | awk '{ byte =$1 /1024/1024/1024; print byte " GB" }')
+REMOTE_AVAILABLE_SPACE=$(grep -oPm1 "(?<=<d:quota-available-bytes>)[^<]+" <<<"$REMOTE_SPACE_INFO" | awk '{ byte =$1 /1024/1024/1024; print byte " GB" }')
 
 echo "Remote available space: $REMOTE_AVAILABLE_SPACE"
-
-exit 1
 
 WEBDAV_FOLDER="$WEBDAVURL/$ARCFOLDERNAME"
 
@@ -36,7 +34,7 @@ else
   exit 1
 fi
 
-for FILES_DIR in $(cat include.list); do
+for FILES_DIR in $(cat "$SCRIPT_DIR/include.list"); do
   ARCHIVE_NAME=$(basename "$(dirname "$FILES_DIR")")_$(basename "$FILES_DIR")_files.tar.gz
 
   ARCHIVE_PATH=$SCRIPT_DIR/$ARCHIVE_NAME
