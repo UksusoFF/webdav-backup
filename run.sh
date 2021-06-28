b12952e20154c779eb93f48457904e0fb55b34fa
Uksusov Kirill <uksusoff@gmail.com>
2021-06-28 12:01:10 +0400

    psql added

--- run.sh
+++ run.sh
@@ -73,6 +73,25 @@ mysql -N -e "SHOW DATABASES;" | grep -v -E "performance_schema|information_schem
   rm "$ARCHIVE_PATH"
 done
 
+su -c "psql -qAtX -c \"SELECT datname FROM pg_database;\"" postgres | grep -v -E "postgres|template0|template1" | while read DB; do
+  ARCHIVE_NAME=${DB}_pg.sql.gz
+
+  ARCHIVE_PATH=$SCRIPT_DIR/$ARCHIVE_NAME
+
+  mysqldump "$DB" | gzip >"$ARCHIVE_PATH"
+
+  RESULT=$(curl --user "$WEB_DAV_USER":"$WEB_DAV_PASS" --digest -T "$ARCHIVE_PATH" "$WEBDAV_FOLDER/$ARCHIVE_NAME" --silent --show-error --write-out '%{http_code}' --output /dev/null)
+
+  if [ "$RESULT" == "201" ]; then
+    echo "Uploaded: $(basename "$ARCHIVE_PATH")"
+  else
+    echo "Failed: $(basename "$ARCHIVE_PATH")"
+    exit 1
+  fi
+
+  rm "$ARCHIVE_PATH"
+done
+
 echo "$ARCFOLDERNAME" >>"$SCRIPT_DIR/previous.list"
 
 head -n -"$ARC_MAX" "$SCRIPT_DIR/previous.list" | while read LASTBACKUPNAME; do
