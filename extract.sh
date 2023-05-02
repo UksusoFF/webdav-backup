79ee9bc848c34d6c2c14b2aecfa8197302a1393c
Kirill Uksusov <k.uksusov@ya.ru>
2023-05-02 16:20:14 +0400

    Unpack script added

--- extract.sh
+++ extract.sh
@@ -11,4 +11,4 @@ TARGET="${2}"
 
 echo "${FILE_NAME}"
 echo "${WEB_DAV_USER}"
-wget --user "${WEB_DAV_USER}" --password "${WEB_DAV_PASS}" "${WEB_DAV_URL}/${FILE_NAME}"
+wget --user "${WEB_DAV_USER}" --password "${WEB_DAV_PASS}" "${WEB_DAV_URL}/${FILE_NAME}" -O - | tar xvz -C "${TARGET}"
