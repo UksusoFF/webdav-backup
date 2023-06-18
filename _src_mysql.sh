bd677636b3d0d5df8c7e2dd5a3b32ea31b71ba5d
Kirill Uksusov <uksusoff@yandex.ru>
2023-06-18 14:35:10 +0400

    MySQL credentials added

--- _src_mysql.sh
+++ _src_mysql.sh
@@ -8,12 +8,19 @@ source "${SCRIPT_DIR}/config.sh"
 
 WEBDAV_FOLDER="${1}"
 
-mysql -N -e "SHOW DATABASES;" | grep -v -E "performance_schema|information_schema|mysql|afterlogic|sys" | while read DB; do
+if [[ -z ${MYSQL_USER} || -z ${MYSQL_PASS} ]]; then
+  echo 'Empty MySQL credentials trying with root'
+  MYSQL_CREDENTIALS=""
+else
+  MYSQL_CREDENTIALS=" --user=${MYSQL_USER} --password=${MYSQL_PASS}"
+fi
+
+mysql -N -e "SHOW DATABASES;" ${MYSQL_CREDENTIALS} | grep -v -E "performance_schema|information_schema|mysql|afterlogic|sys" | while read DB; do
   ARCHIVE_NAME=${DB}_db.sql.gz
 
   ARCHIVE_PATH="${SCRIPT_DIR}/${ARCHIVE_NAME}"
 
-  mysqldump "${DB}" | gzip >"${ARCHIVE_PATH}"
+  mysqldump "${DB}" ${MYSQL_CREDENTIALS} | gzip >"${ARCHIVE_PATH}"
 
   RESULT=$(curl --user "${WEB_DAV_USER}":"${WEB_DAV_PASS}" --digest -T "${ARCHIVE_PATH}" "${WEBDAV_FOLDER}/${ARCHIVE_NAME}" --silent --show-error --write-out '%{http_code}' --output /dev/null)
 
